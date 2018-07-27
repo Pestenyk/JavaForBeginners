fa31e6d041502848d0ad252a867abaef6dc2f889
Pestenyk <pestenyka@gmail.com>
2018-07-28 01:59:33 +0300

    Made some changes

--- Lesson_2.java
+++ Lesson_2.java
@@ -1,24 +1,5 @@
-import java.util.Scanner;
-
-public class Lesson_2 {
+public class Lesson_1 {
 	public static void main (String[] args) {
-
-		int num1 = ReadNumber();
-		int num2 = ReadNumber();
-		int result = sum(num1, num2);
-		System.out.println("Sum = " + result);
-	}
-	
-		public static int ReadNumber() {
-
-			Scanner sc = new Scanner(System.in);
-			System.out.println("¬ведите число: ");
-			return sc.nextInt();
+		System.out.println("Hello World!");
 		}
-
-		public static int sum (int num1, int num2) {
-
-	 		return num1 + num2;
-		}
-	
 }
---
+++ Lesson_3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lesson_2 {
+	public static void main (String[] args) {
+
+		int num1 = ReadNumber();
+		int num2 = ReadNumber();
+		int result = sum(num1, num2);
+		System.out.println("Sum = " + result);
+	}
+	
+		public static int ReadNumber() {
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("¬ведите число: ");
+			return sc.nextInt();
+		}
+
+		public static int sum (int num1, int num2) {
+
+	 		return num1 + num2;
+		}
+	
+}
