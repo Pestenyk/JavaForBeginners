669e6fc6a98293657e68e0e708d20bd60c8839f2
Pestenyk <41734008+Pestenyk@users.noreply.github.com>
2018-07-29 15:31:50 +0300

    Update Lesson_5.java

--- Lesson_5.java
+++ Lesson_5.java
@@ -5,21 +5,21 @@ public class Lesson_5 {
 		sumNum();
 	}
 
-		// A loop that prints numbers from 1 to 22 inclusively to the console
-		public static void printNum() {
-			for(int i = 1; i<= 20; i++) {
-				System.out.print(" " + i);
-			}
-			System.out.println("\n");
+	// A loop that prints numbers from 1 to 22 inclusively to the console
+	public static void printNum() {
+		for(int i = 1; i<= 20; i++) {
+			System.out.print(" " + i);
+		}
+		System.out.println("\n");
 		}
 	
-		// We deduce the sum of numbers from 1 to 112 in steps of 3
-		public static void sumNum() {
-			int result = 0;
-			for(int i = 1; i<=112; i = i + 3) {
-				result = result + i;	
-			}
-		System.out.println("Sum = " + result);
+	// We deduce the sum of numbers from 1 to 112 in steps of 3
+	public static void sumNum() {
+		int result = 0;
+		for(int i = 1; i<=112; i = i + 3) {
+			result = result + i;	
 		}
+	System.out.println("Sum = " + result);
+	}
 }
 			
